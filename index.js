a008e25d7a2d1feb0b16631441573ca890ab7679
aminahagi <amina.hagi@student.moringaschool>
2022-11-24 23:15:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(action="roller-skate") {
+    return `This Saturday, I want to ${action}!`
+}
+function mondayWork(action="go to the office") {
+    return `This Monday, I will ${action}.`
+}
+function wrapAdjective(symbol) {
+    return function wrapper(adj){
+        return `You are ${symbol}${adj}${symbol}!`
+    }
+}
